185797c8c43577828ce1c878abb76c90f8256c74
Syhh885403 <hieuho285@gmail.com>
2023-02-11 08:35:59 -0600

    update

--- src/pages/faq/index.js
+++ src/pages/faq/index.js
@@ -1,57 +1,71 @@
-import React, { useState } from "react"
-import Header from "../../components/Header"
-import { Box, ListItem } from "@mui/material"
-import List from "@mui/material/List"
-import ListItemButton from "@mui/material/ListItemButton"
-import ListItemText from "@mui/material/ListItemText"
-import ExpandLess from "@mui/icons-material/ExpandLess"
-import ExpandMore from "@mui/icons-material/ExpandMore"
-import Collapse from "@mui/material/Collapse"
-import { mockDataFaq } from "../../data/mockData"
+import React, { useState, useContext } from "react";
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import Collapse from "@mui/material/Collapse";
+import { mockDataFaq } from "../../data/mockData";
+import { ColorModeContext } from "../../theme";
 
 function Faq() {
-    const [open, setOpen] = useState(
-        mockDataFaq.map((data) => ({ ...data, open: false }))
-    )
+	const { colors } = useContext(ColorModeContext);
+	const [open, setOpen] = useState(
+		mockDataFaq.map((data) => ({ ...data, open: false }))
+	);
 
-    function handleClick(event) {
-        setOpen((prevData) =>
-            prevData.map((data) => {
-                if (
-                    data.question === event.question &&
-                    data.answer === event.answer
-                ) {
-                    return { ...event, open: !event.open }
-                }
-                return data
-            })
-        )
-    }
+	function handleClick(event) {
+		setOpen((prevData) =>
+			prevData.map((data) => {
+				if (data.question === event.question && data.answer === event.answer) {
+					return { ...event, open: !event.open };
+				}
+				return data;
+			})
+		);
+	}
 
-    const listItems = open.map((data, i) => (
-        <Box display="flex" key={i} >
-            <ListItemButton onClick={() => handleClick(data)}>
-                <ListItemText primary={data.question} />
-                {data.open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-            <Collapse in={data.open} timeout="auto" unmountOnExit>
-                <List component="div" disablePadding>
-                    <ListItemButton sx={{ pl: 4 }}>
-                        <ListItemText primary={data.answer} />
-                    </ListItemButton>
-                </List>
-            </Collapse>
-        </Box>
-    ))
+	const listItems = open.map((data, i) => (
+		<Box
+			bgcolor={colors.grey[800]}
+			display="flex"
+			flexDirection="column"
+			key={i}
+			margin={data.open ? `5px 0` : "0"}
+			borderTop={data.open ? "0" : "1px solid #e5e5e5"}
+		>
+			<ListItemButton onClick={() => handleClick(data)}>
+				<ListItemText
+					sx={{ color: colors.greenAccent[300] }}
+					primary={data.question}
+				/>
+				{data.open ? <ExpandLess /> : <ExpandMore />}
+			</ListItemButton>
+			<Collapse
+				in={data.open}
+				timeout="auto"
+				unmountOnExit
+				sx={{ "& .MuiCollapse-root": { marginBottom: "5px" } }}
+			>
+				<List component="div" disablePadding>
+					<ListItemButton sx={{ pl: 4 }}>
+						<ListItemText primary={data.answer} />
+					</ListItemButton>
+				</List>
+			</Collapse>
+		</Box>
+	));
 
-    return (
-        <Box m="10px 20px">
-            <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
-            <Box height="75vh" m="40px 0 0 0">
-                <List>{listItems}</List>
-            </Box>
-        </Box>
-    )
+	return (
+		<Box m="10px 20px">
+			<Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+			<Box height="75vh" m="40px 0 0 0">
+				<List>{listItems}</List>
+			</Box>
+		</Box>
+	);
 }
 
-export default Faq
+export default Faq;
